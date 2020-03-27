5da8a14b07ed7dd1627b84c97eda4ff8d25a88bf
nasa2135 <nasa2135@gmail.com>
2020-03-27 10:48:45 +0900

    menu edit

--- src/pages/callback.js
+++ src/pages/callback.js
@@ -1,29 +1,42 @@
-import React from "react"
-import { Link } from "gatsby"
-import { logout } from "../utils/auth"
-
+import React from "react";
+import { Link } from "gatsby";
+import { logout } from "../utils/auth";
 
 const Callback = () => {
   return (
     <>
       <p>Callback</p>
       <nav>
-        <Link to="/">Home</Link>{" "}
-        <Link to="/posts">Mark</Link>{" "}
-        <Link to="/tags">tags</Link>{" "}
-        <Link to="/account/">My Account</Link>{" "}
-        <a
-          href="#logout"
-          onClick={e => {
-            logout()
-            e.preventDefault()
-          }}
-        >
-          Log Out
-        </a>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>{" "}
+          </li>
+          <li>
+            <Link to="/posts">Mark</Link>{" "}
+          </li>
+          <li>
+            {" "}
+            <Link to="/tags">tags</Link>{" "}
+          </li>
+          <li>
+            <Link to="/account/">My Account</Link>{" "}
+          </li>
+          <li>
+            {" "}
+            <a
+              href="#logout"
+              onClick={e => {
+                logout();
+                e.preventDefault();
+              }}
+            >
+              Log Out
+            </a>
+          </li>
+        </ul>
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default Callback
+export default Callback;
